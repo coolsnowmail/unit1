f15f575f35b2cd932def3e45256d236edf39a8b5
vurtune <kostya.urenevar@gmail.com>
2020-09-16 19:35:29 +0400

    Initial commit

---
+++ app/src/main/java/com/skill_factory/unit3/adapter/Product.kt
@@ -0,0 +1,4 @@
+package com.skill_factory.unit3.adapter
+
+import androidx.annotation.DrawableRes
+class Product(val id: Int, @DrawableRes val idIcon: Int, val name: String, val desc: String)
---
+++ app/src/main/java/com/skill_factory/unit3/animator/MyItemAnimator.kt
@@ -0,0 +1,12 @@
+package com.skill_factory.unit3.animator
+
+import android.content.Context
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.RecyclerView
+import com.skill_factory.unit3.R
+
+class MyItemAnimator(private val context: Context) : DefaultItemAnimator() {
+
+}
